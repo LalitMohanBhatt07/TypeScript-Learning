722a535daac908b4abd0448376fceb0fcf203ee8
Lb181808 <lb181808@gmail.com>
2024-09-24 23:47:18 +0530

    Array in Typescript

---
+++ src/_6arrayInJavascript.ts
@@ -0,0 +1,9 @@
+function maxValues(arr:number[]):number{
+    let max:number=0;
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
